7e43bae5b274d2f935c3bdecc64ad369496f6322
kanak gupta <kanakgupta120804@gmail.com>
2025-05-03 21:14:41 +0530

    commit

---
+++ src/Component/Login.jsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+
+// function Login() {
+//   return (
+//     <div className='px-[30%]'>
+//         <div className='bg-pink-500 w-[440px] h-[400px] mt-[90px]'>
+//             <div className=''>
+//                 <h1 className='flex justify-center text-2xl'>Login</h1>
+//                 <div>
+//                     <p className='ml-[30px] mt-[10px]'>Email</p>
+//                     <div>
+//                         <input
+//                         value="text"
+//                         placeholder='example@example.com'>
+//                         </input>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Login
+
+
